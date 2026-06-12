7f7cfb1a27f2cbd57c3e5199eadeae97355de224
DirkTomefy <DirkTomefy@gmail.com>
2026-06-12 09:48:46 +0300

    [feat] : ajouter WelcomeServlet
    ce servlet servira de point d'entrée au client pour voir si le framework est bien utilisé ou non

---
+++ src/main/java/com/dirkfw/servlet/WelcomeServlet.java
@@ -0,0 +1,31 @@
+package com.dirkfw.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+
+    public void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Bonjour depuis votre framework préférée !</h1>");
+        out.println("<p>Vous venez de : "+request.getRequestURL().toString()+"</p>");
+        out.println("</body></html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
