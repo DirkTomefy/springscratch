1b4bcf8c1ab28d36b5ae7be3a733f1ed83ac5e8a
DirkTomefy <DirkTomefy@gmail.com>
2026-06-30 10:35:57 +0300

    [fix] : utiliser un attribut static pour l'attribut init CONTROLLER_PACKAGE

--- src/main/java/com/dirkfw/servlet/listener/FrontServletContextListener.java
+++ src/main/java/com/dirkfw/servlet/listener/FrontServletContextListener.java
@@ -12,12 +12,13 @@ import jakarta.servlet.annotation.WebListener;
 public class FrontServletContextListener implements ServletContextListener {
 
     public static final String URL_PROCESSOR_ATTR = "urlProcessor";
+    public static final String CONTROLLER_PACKAGE="CONTROLLER_PACKAGE";
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext context = sce.getServletContext();
 
-        String controllerPackage = context.getInitParameter("CONTROLLER_PACKAGE");
+        String controllerPackage = context.getInitParameter(CONTROLLER_PACKAGE);
         if (controllerPackage == null) {
             controllerPackage = ""; 
         }
